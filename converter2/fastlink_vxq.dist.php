aef4bc017694a75620c92f63edd655965f187795
DiamondVano <vano@diamondc.ru>
2012-07-17 13:47:58 +0600

    добавлена конфигурация по умолчанию для транспорта фл_вхк

---
+++ converter2/fastlink_vxq.dist.php
@@ -0,0 +1,9 @@
+<?php
+DEFINE("_PARTNER_",		"fastlink_vxq");
+DEFINE("_PARTNER_ID_",	3);
+
+DEFINE("_SRC_PATH_","/mnt/blaze/catalog");//путь к оригинальным файлам (aka блэйз)
+DEFINE("_OUT_PATH_","/mnt/ftp/fl_ready");//путь к оригинальным файлам (aka блэйз)
+DEFINE("_MEDIA_PATH_","D:/vano/home/inventory/converter2/content");//путь к файлам в медиакаталоге (aka медиа1)
+
+include ('converter.cfg.php');
